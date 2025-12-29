bb6f4cbc9c03453d4e3fea3f79363f5a2881eff4
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2025-12-29 19:10:09 +0500

    Related jobs

--- lib/components/detailsContainer/DetailsContainer.tsx
+++ lib/components/detailsContainer/DetailsContainer.tsx
@@ -263,9 +263,15 @@ const DetailsContainer = ({ data }: any) => {
 								</section>
 							</Paper>
 						)} */}
-						 {data?.companyDetails?.ceoCompany && (
+						 {data?.companyDetails?.ceoCompany ? (
 							<RelatedJobs companyName={data?.companyDetails?.ceoCompany} currentJobId={data._id} />
-						)} 
+						):<Paper className='details-component-paper'>
+								<section className={styles["job-company-details"]}>
+								     <h2 className="text-md font-semibold text-slate-800 mb-5 flex items-center gap-2">
+										<WorkIcon className="text-blue-600" /> No Related Jobs Found
+									 </h2>
+								</section>
+							</Paper>} 
 						
 					</div>
 					<div className={styles["job-details-bottom-buttons"]}>
